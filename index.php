068ee4e00128e7256676a0dcbb22aa462d6d7ab4
dumyui81 <96933448+dumyui81@users.noreply.github.com>
2022-01-02 22:43:04 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 require 'vendor/autoload.php'; // If you're using Composer (recommended)
 // Comment out the above line if not using Composer
- //require("sendgrid-php/sendgrid-php.php");
+ require("sendgrid-php/sendgrid-php.php");
 // download sendgrid-php.zip from the latest release here,
 // which is included in the download:
 // https://github.com/sendgrid/sendgrid-php/releases
@@ -14,7 +14,7 @@ $email->addContent("text/plain", "and easy to do anywhere, even with PHP");
 $email->addContent(
     "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
 );
-$sendgrid = new \SendGrid('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');
+$sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
 try {
     $response = $sendgrid->send($email);
    // print $response->statusCode() . "\n";
