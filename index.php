f0547fa9c55eaa9c8b76968169148d2093a969a0
dumyui81 <96933448+dumyui81@users.noreply.github.com>
2022-01-02 22:16:41 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-//require 'vendor/autoload.php'; // If you're using Composer (recommended)
+require 'sendgrid/vendor/autoload.php'; // If you're using Composer (recommended)
 // Comment out the above line if not using Composer
  require("sendgrid-php/sendgrid-php.php");
 // If not using Composer, uncomment the above line and
